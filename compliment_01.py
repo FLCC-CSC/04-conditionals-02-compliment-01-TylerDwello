57a60a5a4e032ff449ed1726e3b165dfe8e01759
TylerDwello <tdwello2@fingerlakes.edu>
2025-10-06 02:41:44 +0000

    compliment

--- compliment_01.py
+++ compliment_01.py
@@ -18,20 +18,16 @@ compliment = input('Would you like a compliment? ')
 if compliment == 'yes':   
     print('You have wonderful eyes.')
     print('Thank you for playing.')
- #
-
-    compliment = input('Would you like a compliment? ')
+compliment = input('Would you like a compliment? ')
 if compliment == 'y':   
      print('Thank you for playing.')
-#
-     compliment = input('Would you like a compliment? ')
+compliment = input('Would you like a compliment? ')
 if compliment == 'Yes':   
      print('Thank you for playing.')
-#
-     compliment = input('Would you like a compliment? ')
+compliment = input('Would you like a compliment? ')
 if compliment == 'no':   
      print('Thank you for playing.')
-#
+
 ########### END YER CODE ABOVE THIS LINE ###########
 
     
